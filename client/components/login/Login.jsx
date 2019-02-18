802882f4b9c7395c02a8882c5a5dc0c7676f6d0b
Ondrej Maksi <ondrej.maksi@gmail.com>
2019-02-18 11:06:51 +0100

    login styling

--- client/components/login/Login.jsx
+++ client/components/login/Login.jsx
@@ -2,17 +2,18 @@
 
 import React from 'react'
 
-import { Input, Button } from 'antd'
+import { Input, Button, Alert } from 'antd'
 
 /**
  * @typedef {object} Props
  * @prop {(accessKeyId: string, secretAccessKey: string) => Promise<void>} onLoginSubmit
+ * @prop {string} error
  */
 
 /**
  * @typedef {object} State
- * @prop {string } accessKeyId
- * @prop {string } secretAccessKey
+ * @prop {string} accessKeyId
+ * @prop {string} secretAccessKey
  */
 
 /**
@@ -53,22 +54,36 @@ export default class Login extends React.Component {
             width: 500px;
             margin: 0 auto;
           }
+
+          .small {
+            font-size: smaller;
+          }
         `}</style>
         <h1>s3admin - Login</h1>
+
+        <p className="small">* Access Key Id and Secret Access Key are not permanently stored.</p>
         <Input
           onChange={this.handleInputChange('accessKeyId')}
           value={this.state.accessKeyId}
           placeholder="Access Key ID"
+          autoComplete="on"
         />
         <Input
           onChange={this.handleInputChange('secretAccessKey')}
           value={this.state.secretAccessKey}
           placeholder="Secret Access Key"
+          autoComplete="on"
         />
 
-        <Button type="primary" onClick={this.handleLoginSubmit}>
+        <Button
+          style={{ float: 'right', marginTop: 5 }}
+          type="primary"
+          onClick={this.handleLoginSubmit}
+        >
           Submit
         </Button>
+
+        {this.props.error && <Alert message={this.props.error} type="error" />}
       </div>
     )
   }
